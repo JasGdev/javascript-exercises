90c514211ec272be27d61ef0b09a6a87b7f896ee
Jason <jason13.deng@gmail.com>
2026-01-13 05:31:00 +0900

    Add html for loops and array

--- foundations/01_helloWorld/helloWorld.js
+++ foundations/01_helloWorld/helloWorld.js
@@ -1,5 +1,10 @@
-const helloWorld = function() {
-  return ''
-};
+// Take in an array.
+// For every even number, it will triple it.
+// Then it will sum all those even numbers.
 
-module.exports = helloWorld;
+function sumOfTripledEvens(array){
+  const evenNums = array.filters((num) => num % 2 == 0);
+  const tripleNums = evenNums.map((num) => num = num * 3);
+  const sum = tripleNums.reduce((total, currentItem) => total + currentItem, 0);
+  return sum
+}
