232165549ddc7745a8ba506ddbab188f14c4b46e
Jason <jason13.deng@gmail.com>
2026-01-13 09:40:20 +0900

    Merge files

--- foundations/01_helloWorld/helloWorld.js
+++ foundations/01_helloWorld/helloWorld.js
@@ -3,3 +3,19 @@ const helloWorld = function() {
 };
 
 module.exports = helloWorld;
+
+function checkAge(age) {
+  if (age > 18) {
+    return true;
+  } else {
+    return confirm('Did parents allow you?');
+  }
+}
+
+function checkAge1(age) {
+  return (age > 18 || confirm('Did parents allow you?'))
+}
+
+function checkAge2(age) {
+  return (age > 18) ? true : confirm('Did parents allow you?');
+}
