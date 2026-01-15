40ed5bbf0a162cdabc0de8cd0111c6b474438f07
Jason <jason13.deng@gmail.com>
2026-01-15 16:21:56 +0900

    Finish 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    newStr = str.replaceAll(' ', '')
+                .replaceAll(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '')
+                .toLowerCase();
+    console.log(newStr)
 
+    return newStr === newStr.split('').reverse().join('');
+
+    
 };
 
 // Do not edit below this line
