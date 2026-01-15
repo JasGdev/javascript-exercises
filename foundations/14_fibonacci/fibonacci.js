c9cce32cbcfe90e52adce8ea30cc418300d61e37
Jason <jason13.deng@gmail.com>
2026-01-15 16:38:44 +0900

    Finish 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,4 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(val) {
+    if (val == 0){
+        return 0
+    } else if (val < 0){
+        return 'OOPS'
+    }
+    let i1 = 1;
+    let i2 = 1;
+    for (index = 1; index < val; index++){
+        sum = i1 + i2;
+        i1 = i2;
+        i2 = sum;
+        console.log(i1)
+
+    }
+    return i1
 
 };
 
